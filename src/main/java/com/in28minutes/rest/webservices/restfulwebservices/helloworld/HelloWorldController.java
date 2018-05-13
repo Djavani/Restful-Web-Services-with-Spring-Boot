fa85869a1cddfe2f0ff2666c4d3ee7176a61dd26
djavani <djavanirgomes@gmail.com>
2018-05-12 23:56:02 -0300

    Internacionalizando mensagem de bom dia

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,12 +1,20 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private MessageSource messageSource;
 
 	// GET
 	// URI - /hello-world
@@ -22,7 +30,11 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World Bean");
-
+	}
+	
+	@GetMapping(path = "/hello-world-internationalized")
+	public String worldInternationalize(@RequestHeader(name="Accept-Language", required=false) Locale locale) {
+		return messageSource.getMessage("good.morning.message", null, locale);
 	}
 
 	// hello-world/path-variable/Diego
