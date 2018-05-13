ef6a5e967e16d89c1f8314ca8af5e282ef028c92
djavani <djavanirgomes@gmail.com>
2018-05-13 15:09:38 -0300

    Autenticacao basica com Spring security

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/VersioningController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/VersioningController.java
@@ -6,16 +6,50 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VersioningController {
 
+	// changing the API version with different request methods
 	@GetMapping("v1/person")
 	public PersonV1 personV1() {
-		return new PersonV1("Bob Charlie");		
-		
+		return new PersonV1("Bob Charlie");
+
 	}
-	
+
 	@GetMapping("v2/person")
 	public PersonV2 personV2() {
-		return new PersonV2(new Name("Bob", "Charlie"));		
-		
+		return new PersonV2(new Name("Bob", "Charlie"));
+
+	}
+
+	// changing the API version with URI param
+	@GetMapping(value = "/person/param", params = "version=1")
+	public PersonV1 paramV1() {
+		return new PersonV1("Bob Charlie");
+	}
+
+	@GetMapping(value = "/person/param", params = "version=2")
+	public PersonV2 paramV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+
+	// changing the API version with Header request param
+	@GetMapping(value = "/person/header", headers = "X-API-VERSION=1")
+	public PersonV1 headerV1() {
+		return new PersonV1("Bob Charlie");
+	}
+
+	@GetMapping(value = "/person/header", headers = "X-API-VERSION=2")
+	public PersonV2 headerV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+
+	// changing the API version with Accept header request value
+	@GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v1+json")
+	public PersonV1 producesV1() {
+		return new PersonV1("Bob Charlie");
+	}
+
+	@GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v2+json")
+	public PersonV2 producesV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
 	}
 
 }
