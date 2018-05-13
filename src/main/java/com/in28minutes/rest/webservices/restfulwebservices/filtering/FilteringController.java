5ccab79247ce39f20563610a80b5f85819320e14
djavani <djavanirgomes@gmail.com>
2018-05-13 14:00:15 -0300

    REFACTORING - implementando um metodo apenas para as duas chamadas de filterMaps do endpoint /filtering e /filtering-list

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -17,32 +17,34 @@ public class FilteringController {
 	//field1, field2
 	@GetMapping("/filtering")
 	public MappingJacksonValue retrieveSomeBean() {
-		SomeBean someBean = new SomeBean("value1", "value2", "value3");
 		
-		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field2") ;
+		return FielterMapping("bean", "field1", "field2");	
 		
-		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
-		
-		MappingJacksonValue mapping = new MappingJacksonValue(someBean);
-		mapping.setFilters(filters);
-		
-		return mapping;
 	}
 	
 	//field2, field 3
 	@GetMapping("/filtering-list")
-	public MappingJacksonValue retrieveListOfSomeBean() {
-		List<SomeBean> list = Arrays.asList(new SomeBean("value1", "value2", "value3"),
-				new SomeBean("value1-2", "value2-2", "value3-2"));
-		
-		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field2", "field3") ;
-		
-		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
-		
-		MappingJacksonValue mapping = new MappingJacksonValue(list);
-		mapping.setFilters(filters);
-		
-		return mapping;
+	public MappingJacksonValue retrieveListOfSomeBean() {		
+		return FielterMapping("List", "field2", "field3");
 	}
 
+	//Generic Method do filter mapping fields
+	public MappingJacksonValue FielterMapping(String beanType, String fieldParam1, String fieldParam2) {	
+		
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept(fieldParam1, fieldParam2);		
+		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+		
+		if(beanType == "bean" ) {
+			SomeBean someBean = new SomeBean("value1", "value2", "value3");
+			MappingJacksonValue mapping = new MappingJacksonValue(someBean);
+			mapping.setFilters(filters);		
+			return mapping;
+		}else {
+			List<SomeBean> list = Arrays.asList(new SomeBean("value1", "value2", "value3"),
+					new SomeBean("value1-2", "value2-2", "value3-2"));			
+			MappingJacksonValue mapping = new MappingJacksonValue(list);
+			mapping.setFilters(filters);		
+			return mapping;
+		}
+	}
 }
